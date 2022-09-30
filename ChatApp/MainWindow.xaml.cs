b23fc9cd5c0760d98d44ba1351059f21ddb9c097
Bloodthirsty Snowfla <99435523+MaR1XyAnA@users.noreply.github.com>
2022-09-30 10:23:07 +0300

    лыалы

--- ChatApp/MainWindow.xaml.cs
+++ ChatApp/MainWindow.xaml.cs
@@ -1,23 +1,10 @@
 ﻿using ChatApp.ClassFolder;
 using ChatApp.Viwe.WindowsFolder;
-using Microsoft.Win32.SafeHandles;
 using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
 using System.Net.Http;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ChatApp
 {
