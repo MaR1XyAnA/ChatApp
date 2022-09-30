d753585cf76dbabce283b088f83fd7f0570a0dfb
Bloodthirsty Snowfla <99435523+MaR1XyAnA@users.noreply.github.com>
2022-09-30 09:45:38 +0300

    222

--- ChatApp/Viwe/PageFolder/AuthorizationPage.xaml.cs
+++ ChatApp/Viwe/PageFolder/AuthorizationPage.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using ChatApp.ClassFolder;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,17 +15,34 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static ChatApp.MainWindow;
 
 namespace ChatApp.Viwe.PageFolder
 {
-    /// <summary>
-    /// Логика взаимодействия для AuthorizationPage.xaml
-    /// </summary>
+    //public HttpClient httpClient = new HttpClient(); // Подключили программу к сити интернет
+    //public EmplyeeClass emplyeeClass; // Подключили класс
     public partial class AuthorizationPage : Page
     {
         public AuthorizationPage()
         {
             InitializeComponent();
+        }
+        private async void SignIn(object sender, RoutedEventArgs e) // Ассинхронный метод
+        {
+            //httpClient.DefaultRequestHeaders.Accept.Add
+            //    (new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json")); // WPF дружит с JSON
+            //var Content = new UserData { username = LoginTextBox.Text, password = PasswordPasswordBox.Password };
+            //HttpContent httpContent = new StringContent
+            //    (JsonConvert.SerializeObject(Content), Encoding.UTF8, "application/json"); // подключаем HttpContent
+            //HttpResponseMessage message = await httpClient.PostAsync("http://localhost:50203/api/Login", httpContent);
+            //if (message.IsSuccessStatusCode)
+            //{
+            //    MessageBox.Show("Я куми кудасай");
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Ты умный? \nLOGIN \nили \nPASSWORD неверный");
+            //}
         }
     }
 }
